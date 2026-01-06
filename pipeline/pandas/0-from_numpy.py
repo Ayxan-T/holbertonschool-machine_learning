0500b43b26d8a6f370e81a96d45bb35152f343da
Me <me@example.com>
2026-01-06 12:11:21 +0000

    autocommit

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Module: 0-from_numpy
+    This module is a part of 'Pandas' project.
+"""
+import numpy as np
+import pandas as pd
+
+
+def from_numpy(array):
+    """
+    input:
+        array -- numpy.ndarray
+    returns:
+        df -- pandas.DataFrame
+    """
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    cols_num = array.shape[1]
+    cols = [letter for letter in alphabet[:cols_num]]
+    return pd.DataFrame(array, columns = cols)
