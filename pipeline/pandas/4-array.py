78340e11255cdedd73c8669f2d977e8cde109567
Aykhan <me@example.com>
2026-01-08 07:58:18 +0000

    autocommit

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    Module: 4-array
+    This module is a part of 'Pandas' project.
+"""
+
+
+def array(df):
+    """
+        Converts the last 10 rows of a Data Frame into a numpy array.
+    """
+
+    return df.tail(10).values
