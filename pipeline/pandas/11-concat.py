596ddff2840dbb2b1c5ecf4f6f75f68426f6a382
Aykhan <me@example.com>
2026-01-08 10:51:27 +0000

    autocommit

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    Module: 11-concat
+    This module is a part of 'Pandas' project.
+"""
+
+index = __import__("10-index").index
+import pandas as pd
+
+def concat(df1, df2):
+    """
+        Concatinates two data frames.
+    """
+
+    df1 = index(df1)
+    df2 = index(df2)
+
+    df2 = df2[:1417411921]
+
+    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])   
+
+    return df
