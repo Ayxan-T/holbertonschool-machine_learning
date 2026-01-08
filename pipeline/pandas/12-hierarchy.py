e77a032dee2d8466de15b14a07454eb7046ec3ca
Aykhan <me@example.com>
2026-01-08 12:06:35 +0000

    autocommit

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -13,8 +13,8 @@ def hierarchy(df1, df2):
         Rearranges the hierarchy of the output of index().
     """
 
-    df1 = df1.set_index(df1)
-    df2 = df2.set_index(df2)
+    df1 = index(df1)
+    df2 = index(df2)
     
     df = pd.concat([df2.loc[1417411980:1417417980], df1.loc[1417411980:1417417980]], keys=['bitstamp', 'coinbase'])
 
