7c2eeb513fdcc46d5d53d3718b00b9b1cab1cff1
Aykhan <me@example.com>
2026-01-08 08:25:48 +0000

    autocommit

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -10,7 +10,4 @@ def slice(df):
         Selects every 60th row in the Data Frame.
     """
     
-    sliced_df = df[['High', 'Low', 'Close', 'Volume_BTC']][::60]
-    sliced_df = sliced_df.rename(columns={'Volume_BTC': 'Volume_(BTC)'})
-    return sliced_df
-
+    return df[['High', 'Low', 'Close', 'Volume_BTC']][::60]
