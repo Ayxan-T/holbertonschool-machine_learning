9ef15daceac2492f0e698b5c232e4d0651f55bd1
Aykhan <me@example.com>
2026-01-08 12:19:11 +0000

    autocommit

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -18,7 +18,7 @@ def hierarchy(df1, df2):
 
     df = pd.concat(
         [
-            df2.loc[1417411980:1417417980], 
+            df2.loc[1417411980:1417417980],
             df1.loc[1417411980:1417417980]
         ],
         keys=['bitstamp', 'coinbase']
