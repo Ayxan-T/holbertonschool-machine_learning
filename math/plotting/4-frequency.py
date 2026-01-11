05ba5562dbfb0a6336dc8fa1d92a6f31fd41fba5
Aykhan <me@example.com>
2026-01-11 15:38:34 +0000

    autocommit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -12,6 +12,7 @@ def frequency():
     plt.ylabel("Number of Students")
     plt.title("Project A")
     plt.xlim(0, 100)
+    plt.ylim(0, 30)
 
     bins = np.arange(0, 101, 10)
     plt.hist(student_grades, bins=bins, edgecolor='black')
