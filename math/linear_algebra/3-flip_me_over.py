885769b943b858af365945756dc2dee44d73d464
Aykhan <me@example.com>
2026-01-21 10:54:48 +0000

    autocommit

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module: 3-flip_me_over
+This module is a part of 'Linear Algebra' project.
+"""
+
+
+def matrix_transpose(matrix):
+    """
+    Given a list of lists, returns the transpose of that 'matrix'.
+    """
+    matrix_T = [ [] for _ in range(len(matrix[0])) ]
