c38268ea0b36e7e041cf7689e3e96bf4efc99812
Aykhan <me@example.com>
2026-01-21 10:57:14 +0000

    autocommit

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -10,3 +10,9 @@ def matrix_transpose(matrix):
     Given a list of lists, returns the transpose of that 'matrix'.
     """
     matrix_T = [ [] for _ in range(len(matrix[0])) ]
+
+    for row in matrix:
+        for idx, elm in enumerate(row):
+            matrix_T[idx].append(elm)
+
+    return matrix_T
