75259e659af75a4bdb8514a6bce490576ec0cdba
Aykhan <me@example.com>
2026-01-21 11:22:38 +0000

    autocommit

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,13 +3,21 @@
 Docstring for '5-across_the_planes' module.
 """
 
-import numpy as np
-
 
 def add_matrices2D(mat1, mat2):
     """
     Docstring for 'add_matrices2D' function.
     """
  
-    if np.array(mat1).shape != np.array(mat2).shape:
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
+
+    mat = [[] for _ in range(len(mat1))]
+
+    k = 0
+    for row1, row2 in zip(mat1, mat2):
+        for elm1, elm2 in zip(row1, row2):
+            mat[k].append(elm1 + elm2)
+        k += 1
+
+    return mat
