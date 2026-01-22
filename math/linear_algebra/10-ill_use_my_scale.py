87a1730d5187a6e51a209caf850d913d849a8667
Aykhan <me@example.com>
2026-01-22 16:01:55 +0000

    autocommit

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Docstring for '10-ill_use_my_scale' module
+"""
+
+
+def np_shape(matrix):
+    """
+    Doctring for 'np_shape' function
+    """
+    return matrix.shape
