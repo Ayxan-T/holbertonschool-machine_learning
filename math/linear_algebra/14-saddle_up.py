8b1fa6ab922a33c3b44f17c18443c34555ece936
Aykhan <me@example.com>
2026-01-22 17:00:23 +0000

    autocommit

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Docstring for '14-saddle_up' module
+"""
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Docstring for 'np_matmul' function
+    """
+    return mat1 @ mat2
