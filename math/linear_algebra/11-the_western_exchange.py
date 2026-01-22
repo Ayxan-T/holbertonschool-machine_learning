e580f21cfb2e96588ac1f926c874b1d293d4cd3f
Aykhan <me@example.com>
2026-01-22 16:06:15 +0000

    autocommit

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Doctring for '11-the_western_exchange' module
+"""
+
+
+def np_transpose(matrix):
+    """
+    Docstring for 'np_transpose(matrix)' function
+    """
+    return matrix.T
