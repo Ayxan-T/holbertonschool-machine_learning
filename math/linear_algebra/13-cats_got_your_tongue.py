a0282a3a8b70e66ad26b6025f4444740ec2b37a1
Aykhan <me@example.com>
2026-01-22 16:22:33 +0000

    autocommit

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Docstring for '13-cats_got_your_tongue' module
+"""
+
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    return np.concatenate(mat1, mat2, axis=axis)
