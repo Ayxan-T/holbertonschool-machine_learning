d610f1c6f31de05ac82dc56374af3472bcc9ba2e
Aykhan <me@example.com>
2026-01-22 10:04:01 +0000

    autocommit

---
+++ math/linear_algebra/8-riding_bareback.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Doctring for '7-riding_bareback' module.
+"""
+
+
+def mat_mul(mat1, mat2):
+    """
+    Docstring for 'mat_mul' function.
+    """
+
+    mat1_shape = [len(mat1), len(mat1[0])]
+    mat2_shape = [len(mat2), len(mat2[0])]
+
+    # if cannot be multiplied
+    if mat1_shape[1] != mat2_shape[0]:
+        return None
+
+    mat = [[] for _ in range(mat1_shape[0])]
+
+    for row_mat1_idx, row_mat1 in enumerate(mat1):
+        Sum = 0
+        for row_mat2, row_mat1_elm in zip(mat2, row_mat1):
+            Sum += row_mat2[row_mat1_idx] * row_mat1_elm
+
+        mat[row_mat1_idx].append(Sum)
+        
+    return mat
