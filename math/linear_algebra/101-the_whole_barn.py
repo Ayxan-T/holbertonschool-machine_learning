b33de4edf079377cf8ef6561d1ae56d597d3d69e
Aykhan <me@example.com>
2026-01-24 08:02:47 +0000

    autocommit

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -3,18 +3,39 @@
 Docstring for '101-the_whole_barn' module
 """
 
-import numpy as np
-
 
 def add_matrices(mat1, mat2):
     """
     Docstring for 'add_matrices(mat1, mat2)' function
     """
-    shape_mat1 = numpy.array(mat1).shape
-    shape_mat2 = numpy.array(mat2).shape
+    shape = []
 
-    for mat1_dim, mat2_dim in zip(shape_mat1, shape_mat2):
-        if mat1_dim != mat2_dim:
-            return None
+    m1, m2 = mat1, mat2
+    while True:
+        try:
+            if len(m1) != len(m2):
+                return None
+            else:
+                shape.append(len(m1))
+        except TypeError:
+            break
 
-    return mat1 + mat2
+        m1, m2 = m1[0], m2[0]
+    
+    return add_recur(mat1, mat2);
+
+    
+def add_recur(mat1, mat2):
+    """
+    Helper function: adds two matrices recursively
+    """
+    mat = []
+
+    # if two lists of ints/floats
+    if type(mat1[0]) is not list:
+        for elm1, elm2 in zip(mat1, mat2):
+            mat.append(elm1 + elm2)
+    else:
+        for row1, row2 in zip(mat1, mat2):
+            mat.append(add_recur(row1, row2))
+    return mat
