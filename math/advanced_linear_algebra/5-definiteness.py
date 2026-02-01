b9951b0ad59cb9637efb88c845bf29ba5477a3a5
Aykhan <me@example.com>
2026-02-01 22:08:10 +0000

    autocommit

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -39,7 +39,7 @@ def definiteness(matrix):
     check_successful = True
     for idx, elm in enumerate(dets):
         if (idx % 2 == 0 and elm <= 0) or
-        idx % 2 == 1 and elm >= 0:
+        (idx % 2 == 1 and elm >= 0):
             check_successful = False
             break
     if check_successful:
