081feb2a47e1eb5150c4c15823ce66f2175e9210
Aykhan <me@example.com>
2026-02-01 18:18:42 +0000

    autocommit

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -8,7 +8,7 @@ def determinant(matrix):
     """
     Docstring for 'determinant' function
     """
-    if type(matrix) is not list or type(matrix[0]) is not list:
+    if type(matrix) is not list or len(matrix) == 0 or type(matrix[0]) is not list:
         raise TypeError("matrix must be a list of lists")
 
     # if zero matrix ( [[]] )
