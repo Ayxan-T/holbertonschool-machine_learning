8ff66d798e3e188cec4349b3de48a28790168664
Aykhan <me@example.com>
2026-02-05 12:46:04 +0000

    autocommit

---
+++ math/probability/poisson.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module: poisson
+"""
+
+class Poisson:
+    """
+    Class: Poisson
+    """
+    def __init__(self, data=None, lambtha=1.):
+        if lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        if data is None:
+            self.lambtha = lambtha
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+            # calculating lambtha (average)
+            Sum = 0
+            for elm in data:
+                Sum += elm
+            self.lambtha = Sum / len(data)
