0b470429825c59ebbcc0ab83b35d409b441e1b08
Aykhan <me@example.com>
2026-02-05 12:54:58 +0000

    autocommit

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -8,9 +8,8 @@ class Poisson:
     """
     Class: Poisson
     """
-    data = []
     def __init__(self, data=None, lambtha=1.):
-        data = data
+        self.data = data
         if lambtha <= 0:
             raise ValueError("lambtha must be a positive value")
         if data is None:
