757564f01e6f0c41d0f678fbc6a577c221e23e2a
Aykhan <me@example.com>
2026-02-07 15:30:44 +0000

    autocommit

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -53,6 +53,6 @@ class Normal:
         Method: cdf
         """
         pi = 3.1415926536
-        z = (x - self.mean) / self.stddev
+        z = (x - self.mean) / (self.stddev * 2**0.5)
         return (1 + (2 / pi**0.5) * (z - z**3 / 3 + z**5 / 10 - z**7 / 42 + z**9 / 216)) / 2
 
