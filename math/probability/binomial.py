30e0576e3fb02f47ffcce0492f3c7b3da57a11fd
Aykhan <me@example.com>
2026-02-07 17:25:40 +0000

    autocommit

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -30,18 +30,7 @@ class Binomial:
             p = 1 - mean / var
             n = round(mean / p)
             
-            # p = mean / n
-
-            temp = dict()
-            for elm in data:
-                if temp.get(elm, None) is None:
-                    temp[elm] = 1
-                else:
-                    temp[elm] += 1
-
-            temp = sorted(temp, key=lambda Key: temp[Key], reverse=True)
-            p = temp[0] / n
-
+            p = mean / n
 
 
         self.p = float(p)
