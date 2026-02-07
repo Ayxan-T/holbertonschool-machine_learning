f3198363f21380c4bbd7ec5e2c97f711d782bf06
Aykhan <me@example.com>
2026-02-07 08:07:31 +0000

    autocommit

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -45,4 +45,5 @@ class Normal:
         """
         e = 2.7182818285
         pi = 3.1415926536
-        return (1 / (self.stddev * (2 * pi)**0.5)) * e**(-0.5 * ((x - self.mean) / self.stddev)**2)
+        return (1 / (self.stddev * (2 * pi)**0.5)) *
+               e**(-0.5 * ((x - self.mean) / self.stddev)**2)
