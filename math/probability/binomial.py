fcebd26941fbb6f545c5eeb278319e423af40c79
Aykhan <me@example.com>
2026-02-07 17:16:40 +0000

    autocommit

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -29,6 +29,17 @@ class Binomial:
 
             p = 1 - mean / var
             n = mean / p
+            
+            p = mean / n
+            /*
+            temp = dict()
+            for elm in data:
+                if temp.get(elm, None) is None:
+                    temp[elm] = 1
+                else:
+                    temp[elm] += 1
+            */
+
 
         self.p = float(p)
         self.n = int(n)
