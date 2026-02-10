85894c54587f0674bb971c94f31630f0d9ac9395
Aykhan <me@example.com>
2026-02-10 14:06:31 +0000

    autocommit

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -15,7 +15,8 @@ def likelihood(x, n, P):
         raise ValueError("n must be a positive integer")
 
     if type(x) is not int or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError("x must be an"
+                "integer that is greater than or equal to 0")
 
     if x > n:
         raise ValueError("x cannot be greater than n")
@@ -27,8 +28,9 @@ def likelihood(x, n, P):
         if elm < 0 or elm > 1:
             raise ValueError("All values in P must be in the range [0, 1]")
 
-    combinations = np.math.factorial(n) / (np.math.factorial(n - x) * np.math.factorial(x))
+    combinations = np.math.factorial(n) / \
+        (np.math.factorial(n - x) * np.math.factorial(x))
 
     prob = combinations * (P**x) * ((1 - P)**(n - x))
-    
-    return prob    
+
+    return prob
