e2836f7576c7def9746224f654ff09ca3155faaa
Aykhan <me@example.com>
2026-02-18 09:05:01 +0000

    autocommit

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module: 0-create_confusion
+"""
+
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """
+    Function: create_confusion_matrix
+    """
+
+    conf_mat = np.zeros(labels.shape[1], labels.shape[1])
+
+    true_vals = np.argmax(labels, axis=1)
+    pred_vals = np.argmax(logits, axis=1)
+
+    for t, p, in zip(true_vals, pred_vals):
+        conf_mat[t, p] += 1
+
+    return conf_mat
