1b902e5f8814ad9ab735c168b8d9852cdd98ff35
Aykhan <me@example.com>
2026-02-18 09:36:31 +0000

    autocommit

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module: 1-sensitivity
+"""
+
+import numpy as np
+
+
+def sensitivity(confusion):
+    """
+    Function: sensitivity
+    """
+
+    sensitivities = []
+    for idx in range(confusion.shape[0]):
+        senst = confusion[idx, idx] / confusion[idx].sum()
+        sensitivities.append(senst)
+
+    return sensitivities
