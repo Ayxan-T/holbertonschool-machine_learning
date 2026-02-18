2f24b4059b115c3e4604cc683eac84391d269504
Aykhan <me@example.com>
2026-02-18 09:46:02 +0000

    autocommit

---
+++ supervised_learning/error_analysis/2-precision.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module: 2-precision
+"""
+
+import numpy as np
+
+
+def precision(confusion):
+    """
+    Function: precision
+    """
+
+    precisions = []
+    for col_idx in range(confusion.shape[1]):
+        prec = confusion[col_idx, col_idx] / np.sum(confusion[:, col_idx], axis=1)
+        precisions.append(prec)
+
+    return numpy.array(precisions)
