1c7876a916d587c0084e7a056e75115b8d664e65
Aykhan <me@example.com>
2026-03-02 18:56:56 +0000

    autocommit

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module: 0-neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Class: Neuron
+    """
+    def __init__(self, nx):
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.normal(size=nx)
+        self.b = 0
+        self.A = 0
