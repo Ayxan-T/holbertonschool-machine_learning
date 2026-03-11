aec297a557850a043d921156eb0fd267b7c44b08
Aykhan <me@example.com>
2026-03-11 12:56:01 +0000

    autocommit

---
+++ supervised_learning/keras/6-train.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+Module: 6-train
+"""
+
+import tensorflow.keras as K
+
+
+train_model(network, data,
+            labels, batch_size,
+            epochs, validation_data=None,
+            early_stopping=False,
+            patience=0, verbose=True,
+            shuffle=False):
+    """
+    Function: train_model
+    """
+    if early_stopping and validation_data is not None:
+
+        early_stopping = K.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=patience
+        )
+
+        History = network.fit(
+            x=data,
+            y=labels,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=verbose,
+            shuffle=shuffle,
+            validation_data=validation_data,
+            callbacks=[early_stopping]
+        )
+
+    else:
+        History = network.fit(
+            x=data,
+            y=labels,
+            batch_size=batch_size,
+            epochs=epochs,
+            verbose=verbose,
+            shuffle=shuffle,
+            validation_data=validation_data
+        )
+
+    return History
