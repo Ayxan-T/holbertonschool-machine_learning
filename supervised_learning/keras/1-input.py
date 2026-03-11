4829694e4f4c77d525ce17a20f2f9ed423ed04d8
Aykhan <me@example.com>
2026-03-11 06:59:35 +0000

    autocommit

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -3,50 +3,30 @@
 Module: 1-input
 """
 
+
 import tensorflow.keras as K
 
-
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """
-    Function: build_model
-    """
-
-    input = K.layers.Input(shape=(nx,))
-
+    # 1. Start the chain
+    inputs = K.layers.Input(shape=(nx,))
     reg = K.regularizers.l2(lambtha)
 
-    next_layers = []
-    for i in range(len(layers)):
-        if i == 0:
-            next_layers.append(
-                K.layers.Dense(
-                    layers[i],
-                    activation=activations[i],
-                    kernel_regularizer=reg
-                )(input))
-        elif i == 1:
-            next_layers.append(
-                K.layers.Dense(
-                    layers[i],
-                    activation=activations[i],
-                    kernel_regularizer=reg
-                )(next_layers[i - 1]))
-            next_layers.append(
-                K.layers.Dropout(1 - keep_prob)(next_layers[i])
-            )
-        else:
-            next_layers.append(
-                K.layers.Dense(
-                    layers[i],
-                    activation=activations[i],
-                    kernel_regularizer=reg
-                )(next_layers[i - 2]))
-            next_layers.append(
-                K.layers.Dropout(1 - keep_prob)(next_layers[i])
-            )
+    # 2. 'x' represents the "current" state of the data
+    x = inputs
 
-    model = K.models.Model(inputs=input, outputs=next_layers[-1])
+    for i in range(len(layers)):
+        # Pass 'x' into the new layer and update 'x' to be the result
+        x = K.layers.Dense(
+            layers[i],
+            activation=activations[i],
+            kernel_regularizer=reg
+        )(x)
+
+        # Add Dropout to hidden layers (not the output layer)
+        if i < len(layers) - 1:
+            x = K.layers.Dropout(1 - keep_prob)(x)
+
+    # 3. Keras traces 'x' back to 'inputs' automatically
+    model = K.models.Model(inputs=inputs, outputs=x)
 
     return model
-
-
