723e60d560a781612f6fd8cbb8b97940d7dceae3
Aykhan <me@example.com>
2026-03-11 11:20:29 +0000

    autocommit

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -7,9 +7,9 @@ import tensorflow.keras as K
 
 
 def train_model(
-        network, 
-        data, 
-        labels, 
+        network,
+        data,
+        labels,
         batch_size, epochs, verbose=True, shuffle=False):
     """
     Function: train_model
