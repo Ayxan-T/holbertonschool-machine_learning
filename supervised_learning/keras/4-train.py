fb6ed879ab2b0e7fe564b7f03d7790b30196730e
Aykhan <me@example.com>
2026-03-11 11:14:47 +0000

    autocommit

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module: 4-train
+"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+    """
+    Function: train_model
+    """
+
+    History = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+
+    return History
