83d612935de575bd6b117d2731008e53762c373a
Aykhan <me@example.com>
2026-03-13 07:17:58 +0000

    autocommit

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+testing
+"""
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+    Function: test_model
+    """
+    test_loss, test_acc = network.evaluate(
+        x=data,
+        y=labels,
+        verbose=verbose
+    )
+
+    return test_loss, test_acc
