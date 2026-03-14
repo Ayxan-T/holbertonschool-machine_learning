d34607f284d627f2d52cb11f4f5a5688452d72de
Aykhan <me@example.com>
2026-03-14 11:21:49 +0000

    autocommit

---
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module: 2-shuffle_data
+"""
+
+import numpy as np
+import tensorflow.keras as K
+
+
+def shuffle_data(X, Y):
+    """
+    for X<m, nx>, Y<m, ny>,
+    returns new X and Y shuffled in the same fashion
+    """
+    # np.random.seed(0)
+    return np.random.permutation(X), np.random.permutation(Y)
