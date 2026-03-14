c9e15c019f27ef7dc2ad718a258e1b789347e15f
Aykhan <me@example.com>
2026-03-14 10:39:27 +0000

    autocommit

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module: 0-norm_constants
+"""
+
+
+def normalization_constants(X):
+    """
+    for X<m, nx>,
+    returns mean and stddev for each feature
+    """
+    mean = X.mean(axis=0)
+    stddev = X.stddev(axis=0)
+
+    return mean, stddev
