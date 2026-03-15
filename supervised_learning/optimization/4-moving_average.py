97ae1005be8fb437eb73d2b03c65973fbf612432
Aykhan <me@example.com>
2026-03-15 16:00:41 +0000

    autocommit

--- supervised_learning/optimization/4-moving_average.py
+++ supervised_learning/optimization/4-moving_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module: 
+Module: 4-moving_averag
 """
 
 import numpy as np
@@ -12,17 +12,17 @@ def moving_average(data, beta):
     """
     v = 0  # Initialize the moving average at 0
     ma_corrected = []
-    
+
     for t in range(1, len(data) + 1):
         # 1. Standard EWMA update (The "Raw" value)
         v = (beta * v) + (1 - beta) * data[t-1]
-        
+
         # 2. Calculate Bias Correction factor
         # This gets closer to 1 as t increases
         correction_factor = 1 - (beta**t)
-        
+
         # 3. Apply correction ONLY for the output
         v_corrected = v / correction_factor
         ma_corrected.append(v_corrected)
-        
+
     return ma_corrected
