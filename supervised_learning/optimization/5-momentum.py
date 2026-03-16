a553fed30682ccbcdde70318cf447ff7f1543f6c
Aykhan <me@example.com>
2026-03-16 06:04:08 +0000

    autocommit

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -10,6 +10,6 @@ def update_variables_momentum(alpha, beta1, var, grad, v):
     """
     Function: update_variables_momentum
     """
-    momentum = beta1 * v + (1 - beta1) * var
+    momentum = beta1 * v + (1 - beta1) * grad
     var = var - alpha * momentum
     return var, momentum
