2b8b687cb4a982a77632acda213e4c4bfa6ecb7b
Aykhan <me@example.com>
2026-03-20 16:16:52 +0000

    autocommit

---
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module: 0-l2_reg_cost
+"""
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """
+    Function: l2_reg_cost
+    """
+    L2_reg = 0
+    for i in range(L):
+        key = "W" + (i + 1)
+        L2_reg += weights[key]**2
+
+    return cost / m + lambtha * L2_reg
