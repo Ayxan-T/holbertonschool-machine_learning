ec54e46256870a9d27b0b8693a19725cbd2b799f
Aykhan <me@example.com>
2026-03-22 10:26:03 +0000

    autocommit

---
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module: ble
+"""
+
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, model):
+    """
+    Calculates the total cost for each layer of the network,
+    accounting for L2 regularization.
+    """
+    # Sum all L2 penalty scalars into one scalar
+    l2_total = tf.add_n(model.losses)
+
+    # Add that scalar to the existing cost tensor
+    return cost + l2_total
