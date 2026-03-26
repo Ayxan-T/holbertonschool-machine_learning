09148d31a1c3a640dbbf4f339736c57119e19c1e
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-03-26 18:42:16 +0400

    task 4

---
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Module: 3-convolve_grayscale
+"""
+
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """
+    images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
+        m is the number of images
+        h is the height in pixels of the images
+        w is the width in pixels of the images
+    kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
+        kh is the height of the kernel
+        kw is the width of the kernel
+    padding is either a tuple of (ph, pw), 'same', or 'valid'
+        if 'same', performs a same convolution
+        if 'valid', performs a valid convolution
+        if a tuple:
+            ph is the padding for the height of the image
+            pw is the padding for the width of the image
+    the image is padded with 0s
+    stride is a tuple of (sh, sw)
+        sh is the stride for the height of the image
+        sw is the stride for the width of the image
+    Returns: a numpy.ndarray containing the convolved images
+    """
+    kh, kw = kernel.shape
+    m, h, w = images.shape
+    
+    match padding:
+        case "same":
+            ph = kh // 2
+            pw = kw // 2
+            padded_images = np.pad(images,
+                           ((0,), (ph,), (pw,)), "constant", constant_values=0)
+            oh, ow = h, w
+        case "valid":
+            oh = h - kh + 1
+            ow = w - kw + 1
+        case _:
+            ph, pw = padding
+            padded_images = np.pad(images,
+                           ((0,), (ph,), (pw,)), "constant", constant_values=0)
+            oh = h + 2*ph - kh + 1
+            ow = w + 2*pw - kw + 1
+    
+    convolved = np.zeros((m, oh, ow))
+
+    for i in range(oh):
+        for j in range(ow):
+            convolved[:, i, j] = np. \
+                sum(padded_images[:, i:i+kh, j:j+kw]*kernel, axis=(1, 2))
+
+    return convolved
