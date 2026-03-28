e4c527bc7789cd4878d6aec7888e57bf9c557ddd
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-03-28 17:05:46 +0400

    task 2

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -18,21 +18,20 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     m = cache["A0"].shape[1]
     print(cache['A' + str(L)].shape)
     print(weights['W' + str(L)].shape)
-    print(weights["W" + str(L)].shape)
 
     grads = dict()  # dW's, db's
 
     # Calculate last layer's output (before activation) grad
     dZ_cache = Y * (cache['A' + str(L)] - 1) + (1 - Y) * cache['A' + str(L)]
 
-    grads["dW" + str(L)] = cache['A' + str(L - 1)] * dZ_cache + (lambtha / m) * weights["W" + str(L)] 
-    grads["db" + str(L)] = dZ_cache
+    grads["dW" + str(L)] = np.matmul(dZ_cache, cache['A' + str(L - 1)]) / m + (lambtha / m) * weights["W" + str(L)] 
+    grads["db" + str(L)] = np.average(dZ_cache, axis=1)
 
     for l in range(L-1, 0, -1):
         # Calculate dZ
         dZ_cache = dZ_cache * weights["W" + str(l+1)] * (1 - np.square(cache["A" + str(l)]))
 
-        grads["dW" + str(l)] = np.average(dZ_cache * cache["A" + str(l-1)] + (lambtha / m) * weights["W" + str(l)], axis=1) 
+        grads["dW" + str(l)] = np.matmul(dZ_cache, cache["A" + str(l-1)]) / m + (lambtha / m) * weights["W" + str(l)]
         grads["db" + str(l)] = np.average(dZ_cache, axis=1)
 
     for l in range(1, L):
