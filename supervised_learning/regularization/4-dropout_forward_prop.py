f480e9311edc3d2c5e91cd5c542ba3c318abf820
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-03-29 02:34:28 +0400

    5th task

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -5,21 +5,21 @@ import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
-    """ Function: dropout_forward_prop 
+    """ Function: dropout_forward_prop
     X.shape = (nx, m)
     W.shape = (kx, nx), k being # of neurons
     """
-    cache = { "A0": X }
+    cache = {"A0": X}
     for i in range(1, L):
         W = weights["W" + str(i)]
         b = weights["b" + str(i)]
-        
+
         # 1. Linear
         Z = np.matmul(W, cache["A" + str(i-1)]) + b
-        
+
         # 2. Activation
         A = np.tanh(Z)
-        
+
         # 3. Mask
         mask = np.random.rand(A.shape[0], A.shape[1]) < keep_prob
         cache["D" + str(i)] = mask.astype(int)
@@ -27,13 +27,10 @@ def dropout_forward_prop(X, weights, L, keep_prob):
         # 4. Scale and Apply
         mask = mask.astype(float) / keep_prob
         cache["A" + str(i)] = A * mask
-    
+
     # Calculate final layer output
     Z = np.matmul(weights["W" + str(L)], cache["A" + str(L-1)]) + weights["b" + str(L)]
     Z_exp = np.exp(Z)
     cache["A" + str(L)] = Z_exp / np.sum(Z_exp, axis=0, keepdims=True)
 
     return cache
-
-
-
