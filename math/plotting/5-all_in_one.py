89cdbdf2519e04dbaa213b14ab347daaf5163f07
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-03-29 02:55:19 +0400

    thug

---
+++ math/plotting/5-all_in_one.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" Module: 5-all_in_one """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def all_in_one():
+    """ Function: all_in_one """
+
+    y0 = np.arange(0, 11) ** 3
+
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x1, y1 = np.random.multivariate_normal(mean, cov, 2000).T
+    y1 += 180
+
+    x2 = np.arange(0, 28651, 5730)
+    r2 = np.log(0.5)
+    t2 = 5730
+    y2 = np.exp((r2 / t2) * x2)
+
+    x3 = np.arange(0, 21000, 1000)
+    r3 = np.log(0.5)
+    t31 = 5730
+    t32 = 1600
+    y31 = np.exp((r3 / t31) * x3)
+    y32 = np.exp((r3 / t32) * x3)
+
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+
+    fig = plt.figure(figsize=(8, 10))
+    gs = fig.add_gridspec(3, 2)
+
+    axes = [fig.add_subplot(gs[i, j]) for i in range(2) for j in range(2)]
+    bottom_ax = fig.add_subplot(gs[2, :])
+
+    plt.title("All in One")
+    plt.tight_layout()
+    plt.show()
