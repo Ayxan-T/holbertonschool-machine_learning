ec3d52fd2f56819f12de42a55598cc0ce3605a5c
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-04-10 23:22:57 +0400

    Fix stride unpacking in pool_backward function

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -26,7 +26,7 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     m, h_new, w_new, c = dA.shape
     m, h_prev, w_prev, c = A_prev.shape
     kh, kw = kernel_shape
-    sh, sw = stride.shape
+    sh, sw = stride
 
     dA_prev = np.zeros(dA_prev.shape)
 
