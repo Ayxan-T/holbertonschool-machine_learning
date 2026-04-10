14265325ee4cd915c1e76992b1741395e37d1d9c
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-04-10 23:58:12 +0400

    PEP8

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -11,16 +11,16 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     Args:
         dA (numpy.ndarray): Global gradient of the output of the pooling layer,
             of shape (m, h_new, w_new, c_new).
-        A_prev (numpy.ndarray): Previous layer's activations of shape 
+        A_prev (numpy.ndarray): Previous layer's activations of shape
             (m, h_prev, w_prev, c).
-        kernel_shape (tuple): A tuple of (kh, kw) representing the height 
+        kernel_shape (tuple): A tuple of (kh, kw) representing the height
             and width of the kernel.
-        stride (tuple): A tuple of (sh, sw) representing the vertical 
+        stride (tuple): A tuple of (sh, sw) representing the vertical
             and horizontal stride.
         mode (str): The pooling orientation, either 'max' or 'avg'.
 
     Returns:
-        numpy.ndarray: The partial derivatives with respect to the 
+        numpy.ndarray: The partial derivatives with respect to the
             previous layer (dA_prev).
     """
     m, h_new, w_new, c = dA.shape
@@ -41,12 +41,12 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
                 if mode == 'avg':
                     dA_prev[i, h_start:h_end, w_start:w_end, :] += \
                         dA[i, j, k, :] / (kh * kw)
-                    
+
                 if mode == 'max':
                     pool_window = A_prev[i, h_start:h_end, w_start:w_end, :]
                     max_mask = (pool_window == np.max(pool_window, axis=(0, 1),
                                 keepdims=True))
-                    
+
                     dA_prev[i, h_start:h_end, w_start:w_end, :] += \
                         max_mask * dA[i, j, k, :]
     return dA_prev
