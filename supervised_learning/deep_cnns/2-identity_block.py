3057d7c6021acc4361910030f33ca6a6bd915567
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-04-19 20:49:45 +0400

    PEP8

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -11,7 +11,9 @@ def identity_block(A_prev, filters):
         F11 is the number of filters in the first 1x1 convolution
         F3 is the number of filters in the 3x3 convolution
         F12 is the number of filters in the second 1x1 convolution
-    All convolutions inside the block should be followed by batch normalization along the channels axis and a rectified linear activation (ReLU), respectively.
+    All convolutions inside the block should be followed by batch normalization
+    along the channels axis and a rectified linear activation (ReLU),
+    respectively.
     All weights should use he normal initialization
     The seed for the he_normal initializer should be set to zero
     Returns: the activated output of the identity block
@@ -25,7 +27,7 @@ def identity_block(A_prev, filters):
     X = K.layers.Conv2D(filters=F11, kernel_size=(1, 1),
                         padding='same', kernel_initializer=he_normal)(A_prev)
     X = K.layers.BatchNormalization(axis=3)(X)
-    X = K.layers.Activation('relu')(X)  
+    X = K.layers.Activation('relu')(X)
 
     # Second component of main path
     X = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
@@ -38,7 +40,6 @@ def identity_block(A_prev, filters):
                         padding='same', kernel_initializer=he_normal)(X)
     X = K.layers.BatchNormalization(axis=3)(X)
 
-    # Final step: Add shortcut value to main path, and pass it through a RELU activation
     X = K.layers.Add()([X, X_shortcut])
     X = K.layers.Activation('relu')(X)
 
