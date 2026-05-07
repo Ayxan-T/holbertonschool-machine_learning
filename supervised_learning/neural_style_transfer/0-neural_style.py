6e4df642fd933c7a8f9f6059c74e3ce0aa50ab9b
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-05-07 15:20:47 +0400

    refactor NST class to enhance type checking for style and content images

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -5,16 +5,19 @@ import numpy as np
 import tensorflow as tf
 
 class NST:
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                    'block4_conv1', 'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        if len(style_image.shape) != 3 or style_image.shape[2] != 3:
+        if not isinstance(style_image, np.ndarray) or \
+        len(style_image.shape) != 3 or style_image.shape[2] != 3:
             raise TypeError("style_image must be a numpy.ndarray with shape"\
                             " (h, w, 3)")
         sh, sw, _ = style_image.shape
 
-        if len(content_image.shape) != 3 or content_image.shape[2] != 3:
+        if not isinstance(content_image, np.ndarray) or \
+        len(content_image.shape) != 3 or content_image.shape[2] != 3:
             raise TypeError("content_image must be a numpy.ndarray with shape"\
                             " (h, w, 3)")
         ch, cw, _ = content_image.shape
