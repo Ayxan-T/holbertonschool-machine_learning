c0d183cc378e9e2e30ec6519d9036f01c30d9b02
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-05-07 15:34:43 +0400

    refactor NST class to enhance type checking for alpha and beta parameters

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -22,9 +22,9 @@ class NST:
                             " (h, w, 3)")
         ch, cw, _ = content_image.shape
 
-        if not isinstance(alpha, float) or alpha < 0:
+        if not isinstance(alpha, (int, float)) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
-        if not isinstance(beta, float) or beta < 0:
+        if not isinstance(beta, (int, float)) or beta < 0:
             raise TypeError("beta must be a non-negative number")
 
         self.style_image = self.scale_image(style_image)
