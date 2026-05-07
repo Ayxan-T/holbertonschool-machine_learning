4d3a5faaa3014d1a3c0aaa83362a9dc76ced1fd5
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-05-07 15:47:18 +0400

    PEP8

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -4,20 +4,22 @@
 import numpy as np
 import tensorflow as tf
 
+
 class NST:
+    """ Class: NST (Neural Style Transfer) """
     style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
                     'block4_conv1', 'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
-        if not isinstance(style_image, np.ndarray) or \
-        len(style_image.shape) != 3 or style_image.shape[2] != 3:
+        if not isinstance(style_image, np.ndarray) \
+                or len(style_image.shape) != 3 or style_image.shape[2] != 3:
             raise TypeError("style_image must be a numpy.ndarray with shape"\
                             " (h, w, 3)")
         sh, sw, _ = style_image.shape
 
-        if not isinstance(content_image, np.ndarray) or \
-        len(content_image.shape) != 3 or content_image.shape[2] != 3:
+        if not isinstance(content_image, np.ndarray) \
+                or len(content_image.shape) != 3 or content_image.shape[2] != 3:
             raise TypeError("content_image must be a numpy.ndarray with shape"\
                             " (h, w, 3)")
         ch, cw, _ = content_image.shape
@@ -35,7 +37,7 @@ class NST:
     @staticmethod
     def scale_image(image):
         if not isinstance(image, np.ndarray) or len(image.shape) != 3 \
-            or image.shape[2] != 3:
+                or image.shape[2] != 3:
             raise TypeError("image must be a numpy.ndarray with shape"\
                             " (h, w, 3)")
 
@@ -47,7 +49,6 @@ class NST:
         )
 
         new_image = new_image[tf.newaxis, ...]
-
 
         # preventing bicubic 'overshoot'
         return tf.clip_by_value(new_image, 0, 255) / 255.0
