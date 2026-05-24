6835ab44ce4d62f137e0e44004ffedb928641dda
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-05-24 15:43:17 +0400

    PEP8

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -21,6 +21,9 @@ def initialize(X, k):
         centroids: a numpy.ndarray of shape (k, num_dimensions) containing
             the initialized centroids for each cluster, or None on failure
     """
+    if k is str:
+        return None
+
     if k <= 0:
         return None
 
