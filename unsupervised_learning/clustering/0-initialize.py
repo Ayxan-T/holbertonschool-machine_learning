d6826427eb642df146e2f537febb6c23b9f0f4d3
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-05-24 15:35:08 +0400

    PEP8

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -30,7 +30,7 @@ def initialize(X, k):
         d = X.shape[1]
 
         centroids = np.random.uniform(low=lows, high=highs, size=(k, d))
-    except (np.core._exceptions.UFuncTypeError, IndexError):
+    except (np.core._exceptions.UFuncTypeError, IndexError, ValueError):
         centroids = None
 
     return centroids if centroids is not None else None
