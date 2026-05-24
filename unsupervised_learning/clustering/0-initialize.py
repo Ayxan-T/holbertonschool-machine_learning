a6bae58a406db3693fef6734aed418f6f4e0098a
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-05-24 15:27:06 +0400

    fix: enhance k value validation

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -21,7 +21,7 @@ def initialize(X, k):
         centroids: a numpy.ndarray of shape (k, num_dimensions) containing
             the initialized centroids for each cluster, or None on failure
     """
-    if k <= 0:
+    if k is not int or k <= 0:
         return None
     
     try:
