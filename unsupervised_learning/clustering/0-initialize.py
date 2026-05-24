3a218a864e1b8f7abc3aca2cb81e88ead314a333
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-05-24 15:30:54 +0400

    PEP8

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""" This module provides a function, which initializes cluster centroids for 
-K-means. 
+""" This module provides a function, which initializes cluster centroids for
+K-means.
 
 Function: initialize()
 """
@@ -10,27 +10,27 @@ import numpy as np
 
 def initialize(X, k):
     """ Initializes cluster centroids for K-means using multivariate uniform
-    distribution. 
-    
+    distribution.
+
     Args:
-        X: a numpy.ndarray of shape (num_samples, num_dimensions) containing 
+        X: a numpy.ndarray of shape (num_samples, num_dimensions) containing
             the dataset that will be used for clustering
         k: a positive integer containing the number of clusters
-    
+
     Returns:
         centroids: a numpy.ndarray of shape (k, num_dimensions) containing
             the initialized centroids for each cluster, or None on failure
     """
     if int(k) <= 0:
         return None
-    
+
     try:
         lows = np.min(X, axis=0)
         highs = np.max(X, axis=0)
         d = X.shape[1]
 
         centroids = np.random.uniform(low=lows, high=highs, size=(k, d))
-    except:
+    except ValueError:
         centroids = None
 
     return centroids if centroids is not None else None
