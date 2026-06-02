59faf64df2ba7ad1d5336959aabfe8c728368a78
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-06-02 17:02:47 +0400

    PEP8 & Docs

--- supervised_learning/decision_tree/6-build_decision_tree.py
+++ supervised_learning/decision_tree/6-build_decision_tree.py
@@ -157,10 +157,13 @@ class Node:
             is_small_enough(x)
         )
 
-    def pred(self,x):
+    def pred(self, x):
+        """
+        Function: pred
+        """
         if x[self.feature] > self.threshold:
             return self.left_child.pred(x)
-        else :
+        else:
             return self.right_child.pred(x)
 
 
@@ -202,6 +205,9 @@ class Leaf(Node):
         pass
 
     def pred(self, x):
+        """
+        Function: pred
+        """
         return self.value
 
 
@@ -249,16 +255,19 @@ class Decision_Tree():
         Function: update_bounds
         """
         self.root.update_bounds_below()
-    
+
     def update_predict(self):
         """
         Function: update_predict
         """
         self.update_bounds()
-        leaves=self.get_leaves()
+        leaves = self.get_leaves()
         for leaf in leaves:
             leaf.update_indicator()
         self.predict = lambda A: [self.pred(individual) for individual in A]
-    
+
     def pred(self, x):
-            return self.root.pred(x)
+        """
+        Function: pred
+        """
+        return self.root.pred(x)
