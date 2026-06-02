56b23b0592869452f7d7f465f0630f07489175ab
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-06-02 16:13:34 +0400

    PEP8

--- supervised_learning/decision_tree/5-build_decision_tree.py
+++ supervised_learning/decision_tree/5-build_decision_tree.py
@@ -137,7 +137,9 @@ class Node:
             child.update_bounds_below()
 
     def update_indicator(self):
-
+        """ 
+        Function: update_indicator
+        """
         def is_large_enough(x):
             return np.logical_and.reduce([
                 x[:, key] > self.lower[key]
