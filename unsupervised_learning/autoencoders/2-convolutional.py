dda95b7f2c3ba4fe01974d38b14639e480329302
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-06-05 18:11:12 +0400

    PEP8

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -16,7 +16,7 @@ def autoencoder(input_dims, filters, latent_dims):
 
     Each convolution in the decoder, except for the last two, uses
     a filter size of (3, 3) with same padding and relu activation,
-    followed by upsampling of size (2, 2). The second to last convolution 
+    followed by upsampling of size (2, 2). The second to last convolution
     use valid padding. The last convolution should have the same number
     of filters as the number of channels in input_dims with sigmoid activation
     and no upsampling.
@@ -31,7 +31,7 @@ def autoencoder(input_dims, filters, latent_dims):
             convolutional layer in the encoder, respectively
         latent_dims:  a tuple of integers containing the dimensions
             of the latent space representation
-    
+
     Returns:
         encoder: the encoder model
         decoder: the decoder model
@@ -46,19 +46,19 @@ def autoencoder(input_dims, filters, latent_dims):
             num_filters, (3, 3), padding='same', activation='relu'
         )(x)
         x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
-    
+
     encoder_output = x
     encoder = keras.models.Model(encoder_input, encoder_output)
 
     decoder_input = keras.layers.Input(shape=latent_dims)
     x = decoder_input
-    
+
     for num_filters in reversed(filters[1:]):
         x = keras.layers.Conv2D(
             num_filters, (3, 3), padding='same', activation='relu'
         )(x)
         x = keras.layers.UpSampling2D((2, 2))(x)
-    
+
     # second to last layer
     x = keras.layers.Conv2D(
         filters[0], (3, 3), padding='valid', activation='relu'
