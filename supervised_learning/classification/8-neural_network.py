813f3be846266dc2d05f0661ef859c1a6454e121
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-06-07 23:24:50 +0400

    feat: implement NeuralNetwork class for binary classification with validation

---
+++ supervised_learning/classification/8-neural_network.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" This module provides a class  that defines a neural network with one hidden
+layer performing binary classification.
+
+Class: NeuralNetwork
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Class: NeuralNetwork """
+    def __init__(self, nx, nodes):
+
+        # Validate nx
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        # Validate nodes
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Initialize parameters for hidden layer
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros(size=(nodes, 1))
+        self.A1 = np.zeros(size=(nodes, 1)) # activations
+
+        # Initialize parameters for output neuron
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
