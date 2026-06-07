2792fc643958afe93ce08469ec4174a6d98ff655
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-06-07 23:40:54 +0400

    feat: add NeuralNetwork class for binary classification with validation

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+""" This module provides a class  that defines a neural network with one hidden
+layer performing binary classification.
+
+Class: NeuralNetwork
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Class: NeuralNetwork """
+    def __init__(self, nx, nodes):
+
+        # Validate nx
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        # Validate nodes
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Initialize private parameters in hidden layer
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros(shape=(nodes, 1))
+        self.__A1 = 0     # activations
+
+        # Initialize private parameters in output neuron
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+        @property
+        def W1(self):
+            return self.__W1
+        
+        @property
+        def b1(self):
+            return self.__b1
+        
+        @property
+        def A1(self):
+            return self.__A1
+        
+        @property
+        def W2(self):
+            return self.__W2
+        
+        @property
+        def b2(self):
+            return self.__b2
+        
+        @property
+        def A2(self):
+            return self.__A2
