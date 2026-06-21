260fd83773be293417e5577c4135496d120a3c16
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-06-21 21:48:00 +0400

    feat: implement forward propagation function for simple RNN

---
+++ supervised_learning/RNNs/1-rnn.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""1-rnn module
+
+Provides a function that performs forward propagation for a simple RNN."""
+
+import numpy as np
+
+
+def rnn(rnn_cell, X, h_0):
+    """Performs forward propagation for a simple RNN.
+    
+    Args:
+        rnn_cell: RNNCell
+        X: np.ndarray(t, m, i) -the data to be used
+        h_0: np.ndarray(m, h) -initial state
+
+    Returns:
+        H: numpy.ndarray -all the hidden states
+        Y: numps.ndarray -all the outputs
+    """
+
+    H = [h_0]
+    Y = []
+
+    for i, input in enumerate(X):
+        h_new, y_new = rnn_cell.forward(H[i], input)
+        H.append(h_new)
+        Y.append(y_new)
+
+    H.pop(0)
+
+    return np.ndarray(H), np.ndarray(Y)
