5cee256c88bac2b97bdc73dd567dd0a28a99c9bd
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-06-21 22:59:13 +0400

    Implement LSTMCell class with forward propagation method

---
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""3-lstm_cell module
+
+Provides a class that represents an LSTM cell."""
+
+import numpy as np
+
+
+class LSTMCell:
+    """A class that represents an LSTM unit."""
+
+    def __init__(self, i, h, o):
+        """Initialize the LSTM cell weights and biases."""
+        self.Wf = np.random.normal(size=(i + h, h))
+        self.Wu = np.random.normal(size=(i + h, h))
+        self.Wc = np.random.normal(size=(i + h, h))
+        self.Wo = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(h, o))
+
+        self.bf = np.zeros((1, h))
+        self.bu = np.zeros((1, h))
+        self.bc = np.zeros((1, h))
+        self.bo = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, c_prev, x_t):
+        """Perform forward propagation for one time step."""
+        concat = np.concatenate((h_prev, x_t), axis=1)
+
+        f = 1 / (1 + np.exp(-(np.matmul(concat, self.Wf) + self.bf)))
+        u = 1 / (1 + np.exp(-(np.matmul(concat, self.Wu) + self.bu)))
+        c_bar = np.tanh(np.matmul(concat, self.Wc) + self.bc)
+        c_next = f * c_prev + u * c_bar
+        o = 1 / (1 + np.exp(-(np.matmul(concat, self.Wo) + self.bo)))
+        h_next = o * np.tanh(c_next)
+
+        y_linear = np.matmul(h_next, self.Wy) + self.by
+        y_exp = np.exp(y_linear - np.max(y_linear, axis=1, keepdims=True))
+        y = y_exp / np.sum(y_exp, axis=1, keepdims=True)
+
+        return h_next, c_next, y
