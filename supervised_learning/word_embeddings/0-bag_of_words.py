9c586cc0e3c9e25817a0550b079c44017cb9aa44
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-07-05 20:05:35 +0400

    Refactor bag_of_words function to return features as a numpy array instead of a list

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -31,6 +31,6 @@ def bag_of_words(sentences, vocab=None):
     
 
     embeddings = vectorizer.fit_transform(sentences).toarray()
-    features = vectorizer.get_feature_names_out().tolist()
+    features = vectorizer.get_feature_names_out()
     
     return embeddings, features
