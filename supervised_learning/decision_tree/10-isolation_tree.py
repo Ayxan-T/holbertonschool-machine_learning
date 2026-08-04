29d3c702af4ae1ada88ecff6021eb747c9e10bda
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-08-04 13:24:56 +0400

    fix: update update_predict method to populate predict attribute

--- supervised_learning/decision_tree/10-isolation_tree.py
+++ supervised_learning/decision_tree/10-isolation_tree.py
@@ -47,6 +47,9 @@ class Isolation_Random_Tree:
         for leaf in leaves:
             leaf.update_indicator()
 
+        # predict all individuals in A
+        self.predict = lambda A: [self.root.pred(individual) for individual in A]
+
     def np_extrema(self, arr):
         """Return the minimum and maximum values of an array."""
         return np.min(arr), np.max(arr)
@@ -113,33 +116,6 @@ class Isolation_Random_Tree:
         else:
             node.right_child = self.get_node_child(node, right_population)
             self.fit_node(node.right_child)
-
-    def predict(self, explanatory):
-        """Predict labels for one or more samples using the trained forest.
-
-        Args:
-            explanatory: A 1D or 2D array of feature values for the samples
-                to classify.
-
-        Returns:
-            A NumPy array containing the predicted label for each sample.
-        """
-        if explanatory.ndim == 1:
-            explanatory = explanatory.reshape(1, -1)
-
-        if not self.numpy_preds:
-            raise ValueError("The forest has not been trained yet.")
-
-        tree_preds = np.array([
-            tree_predict(explanatory) for tree_predict in self.numpy_preds
-        ])
-
-        preds = []
-        for i in range(tree_preds.shape[1]):
-            values, counts = np.unique(tree_preds[:, i], return_counts=True)
-            preds.append(values[np.argmax(counts)])
-
-        return np.array(preds)
 
     def fit(self, explanatory, verbose=0):
         """Fit the isolation tree to the explanatory data."""
