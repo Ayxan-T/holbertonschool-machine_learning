91ffcf926704c7ec8b47861167f4a1cbb8c58a41
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-08-12 14:59:26 +0400

    feat: implement epsilon-greedy action selection function

---
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Module: 2-epsilon_greedy"""
+
+import numpy as np
+
+
+def epsilon_greedy(Q, state, epsilon):
+    """Performs epsilon-greedy to determine next action.
+    
+    Args:
+        Q: (numpy.ndarray) the q-table
+        state: current state
+        epislon
+    
+    Returns:
+        action_idx: index of next action
+    """
+
+    num_actions = Q.shape[1]
+    p = np.random.uniform()
+    if p > epsilon:
+        # find idx of best action col for state
+        action_idx = np.argmax(Q[state, :], axis=1)
+    else:
+        action_idx = np.random.randint(num_actions)
+
+    return action_idx
