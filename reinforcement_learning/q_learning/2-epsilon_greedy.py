f710a76bbed9da6b4d408b3e9c4f93ced688b20b
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-08-12 15:02:29 +0400

    style: resolve PEP8 formatting issues

--- reinforcement_learning/q_learning/2-epsilon_greedy.py
+++ reinforcement_learning/q_learning/2-epsilon_greedy.py
@@ -6,12 +6,12 @@ import numpy as np
 
 def epsilon_greedy(Q, state, epsilon):
     """Performs epsilon-greedy to determine next action.
-    
+
     Args:
         Q: (numpy.ndarray) the q-table
         state: current state
         epislon
-    
+
     Returns:
         action_idx: index of next action
     """
