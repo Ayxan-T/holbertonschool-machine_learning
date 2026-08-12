452d8932b51fcf4e9b63498b51c9752cae12eb7d
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-08-12 14:08:36 +0400

    style: resolve PEP8 formatting issues

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -5,14 +5,14 @@ import numpy as np
 
 
 def q_init(env):
-	"""Initialize the Q-table for a FrozenLake environment.
+    """Initialize the Q-table for a FrozenLake environment.
 
-	Args:
-		env: The FrozenLakeEnv instance.
+    Args:
+        env: The FrozenLakeEnv instance.
 
-	Returns:
-		numpy.ndarray (n_observations, n_actions): Q-table initialized to zeros
-	"""
-	return np.zeros((env.observation_space.n, env.action_space.n))
+    Returns:
+        numpy.ndarray (n_observations, n_actions): Q-table initialized to zeros
+    """
+    return np.zeros((env.observation_space.n, env.action_space.n))
 
 
