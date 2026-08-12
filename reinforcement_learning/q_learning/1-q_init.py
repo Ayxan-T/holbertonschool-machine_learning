a71a2abfc43b536c06f16ff5a9b2d1bc458abd0a
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-08-12 14:06:17 +0400

    feat: add function to initialize Q-table for FrozenLake environment

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Module: 1-q_init"""
+
+import gymnasium as gym
+import numpy as np
+
+
+def q_init(env):
+	"""Initialize the Q-table for a FrozenLake environment.
+
+	Args:
+		env: The FrozenLakeEnv instance.
+
+	Returns:
+		numpy.ndarray (n_observations, n_actions): Q-table initialized to zeros
+	"""
+	return np.zeros((env.observation_space.n, env.action_space.n))
+
+
