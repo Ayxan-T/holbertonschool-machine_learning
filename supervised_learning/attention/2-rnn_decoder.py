085e6050d88ecd9fb9d1d6ccc58062e61d047cf0
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-08-17 21:51:14 +0400

    refactor: modify GRU definition to also return output sequences

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -14,6 +14,7 @@ class RNNDecoder(tf.keras.layers.Layer):
         self.attention = SelfAttention(units)
         self.gru = tf.keras.layers.GRU(
                 units,
+                return_sequences=True,
                 return_state=True,
                 recurrent_initializer="glorot_uniform"
             )
