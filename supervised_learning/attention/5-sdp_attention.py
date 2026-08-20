fix: update dk_root calculation in sdp_attention function

supervised_learning/attention/5-sdp_attention.py
@@ -10,7 +10,7 @@ def sdp_attention(Q : tf.Tensor, K : tf.Tensor, V : tf.Tensor, mask=None):
         K: (..., seq_len_v, dk)
         V: (..., seq_len_v, dv)
     """
-    dk_root = tf.math.sqrt(Q.shape[-1])
+    dk_root = tf.shape(Q)[-1] ** 0.5
 
     scores = tf.matmul(Q, K, transpose_b=True)  # (..., seq_len_q, seq_len_v)
 