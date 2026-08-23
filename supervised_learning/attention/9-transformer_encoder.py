5a30a26215ce263b5f8be5841551fbf908cae09a
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-08-23 22:55:07 +0400

    style: resolve PEP8 issue

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -29,7 +29,7 @@ class Encoder(tf.keras.layers.Layer):
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
 
         # 1. Add positional encoding up to input_seq_len
-        # Cast to x.dtype in case x is float32/float64 and positional_encoding 
+        # Cast to x.dtype in case x is float32/float64 and positional_encoding
         # is float64/float32
         pos_encoding = tf.cast(
             self.positional_encoding[:seq_len, :], dtype=x.dtype)
