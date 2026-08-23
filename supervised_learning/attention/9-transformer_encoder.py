dc5bc64b1673a845b42b1229915e7c8bdf84fa1d
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-08-23 22:24:27 +0400

    trying

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -26,7 +26,7 @@ class Encoder(tf.keras.layers.Layer):
         Args: x, training, mask"""
         seq_len = tf.shape(x)[1]
 
-        x = self.embedding(x)
+        # x = self.embedding(x)
 
         # 1. Add positional encoding up to input_seq_len
         # Cast to x.dtype in case x is float32/float64 and positional_encoding is float64/float32
