11d549ed9f092c4afa620a6a69e9dac3e9b6e4ba
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-08-23 20:53:02 +0400

    feat: implement decoder block for transformer architecture

---
+++ supervised_learning/attention/8-transformer_decoder_block.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""Module: 8-transformer_decoder_block"""
+
+import tensorflow as tf
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
+
+
+class DecoderBlock(tf.keras.layers.Layer):
+    """Decoder block of transformer architecture.
+    Method: call"""
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        super().__init__()
+
+        self.mha1 = MultiHeadAttention(dm, h)
+        self.mha2 = MultiHeadAttention(dm, h)
+
+        self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
+        self.dense_output = tf.keras.layers.Dense(dm)
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout3 = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
+        """Perform decoder block operations on input x."""
+        # Masked Multi-Head Self-Attention (Target sequence attends to itself)
+        attn1, weights_block1 = self.mha1(x, x, x, mask=look_ahead_mask)
+        attn1 = self.dropout1(attn1, training=training)
+        out1 = self.layernorm1(x + attn1)
+
+        # 2. Cross-Attention (Queries from decoder, Keys & Values from encoder)
+        attn2, weights_block2 = self.mha2(out1, encoder_output, encoder_output, mask=padding_mask)
+        attn2 = self.dropout2(attn2, training=training)
+        out2 = self.layernorm2(out1 + attn2)
+
+        # 3. Position-wise Feed-Forward Network
+        ffn_output = self.dense_hidden(out2)
+        ffn_output = self.dense_output(ffn_output)
+        ffn_output = self.dropout3(ffn_output, training=training)
+        out3 = self.layernorm3(out2 + ffn_output)
+
+        return out3, weights_block1, weights_block2
