fe73ad1f9481bbc7461a6a25793cbbbf833e172f
Ayxan-T <ayxan.tagiyev14@gmail.com>
2026-09-10 12:44:04 +0400

    refactor: use custom-defined initializer for dense layer

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -6,9 +6,15 @@ import tensorflow as tf
 
 def dropout_create_layer(prev, n, activation, keep_prob, training=True):
     """Creates a layer of a neural network using dropout."""
+    init = tf.keras.initializers.VarianceScaling(
+        scale=2.0,
+        mode="fan_avg"
+    )
+
     layer = tf.keras.layers.Dense(
         units=n,
-        activation=activation
+        activation=activation,
+        kernel_initializer=init
     )
 
     tensor = layer(prev)
